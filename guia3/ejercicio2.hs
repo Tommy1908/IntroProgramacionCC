ebb981a6f0a6ec5fe93109d986f7dba3700aa187
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-09 16:01:38 -0300

    Ejer 1 y 2

--- guia3/ejercicio2.hs
+++ guia3/ejercicio2.hs
@@ -1,3 +1,26 @@
+--2)Especificar e implementar las siguientes funciones, incluyendo su signatura.
+
+{-A) 
+problema absoluto (x: Z): N {
+    Requiere:{true}
+    Asegura:{res = |x|}
+-}
+absoluto:: Int -> Int
+absoluto 0 = 0
+absoluto x| x < 0 = -x
+          | otherwise = x  
+
+{-B)
+problema maximoabsoluto (x,y: Z): N {
+    Requiere:{True}
+    Asegura:{res = el mayor absoluto}
+}
+-}
+maximoAbsoluto:: Int -> Int -> Int
+maximoAbsoluto x y| absoluto x >= absoluto y = absoluto x
+                  | otherwise = absoluto y
+
+
 maximo3:: Int -> Int -> Int -> Int
 maximo3 x y z| x >= y && x >= z = x 
              | y >= z = y --sabemos x no es maximo
