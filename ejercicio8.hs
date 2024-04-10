cc0d2eef0ee54940431342b626a7252c897b9055
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-10 14:08:18 -0300

    ejercicio 8

---
+++ ejercicio8.hs
@@ -0,0 +1,24 @@
+{--Ejercicio 8. Implementar una funci´on comparar :: Integer ->Integer ->Integer
+problema comparar (a:Z, b:Z) : Z {
+requiere: {T rue}
+asegura: {(res = 1 ↔ sumaU ltimosDosDigitos(a) < sumaU ltimosDosDigitos(b))}
+asegura: {(res = −1 ↔ sumaU ltimosDosDigitos(a) > sumaU ltimosDosDigitos(b))}
+asegura: {(res = 0 ↔ sumaU ltimosDosDigitos(a) = sumaU ltimosDosDigitos(b))}
+}
+problema sumaUltimosDosDigitos (x: Z) : Z {
+requiere: {T rue}
+asegura: {res = (|x| m´d 10) + (⌊(|x|/10)⌋ mod 10)}
+}
+Por ejemplo:
+comparar 45 312 ⇝ -1 porque 45 ≺ 312 y 4 + 5 > 1 + 2.
+comparar 2312 7 ⇝ 1 porque 2312 ≺ 7 y 1 + 2 < 0 + 7.
+comparar 45 172 ⇝ 0 porque no vale 45 ≺ 172 ni tampoco 172 ≺ 45.--}
+
+sumaUltimosDosDigitos :: Int -> Int
+sumaUltimosDosDigitos x =  (mod (absoluto x) 10) + (mod ( (div (absoluto x) 10)) 10)
+
+
+absoluto:: Int -> Int
+absoluto 0 = 0
+absoluto x| x < 0 = -x
+          | otherwise = x 
