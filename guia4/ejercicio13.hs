74587f2e071e627dade742dd7820c6329cf1e2bf
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-17 13:48:07 -0300

    ejercicio 13

--- guia4/ejercicio13.hs
+++ guia4/ejercicio13.hs
@@ -9,9 +9,10 @@ fn :: Int -> Int -> Int
 fn 1 m = fm 1 m
 fn n m = fm i m + fn (i-1) m
         where i = n
-
+--fn se repite n veces, y llama fn, pasandole cada iteracion un i mas chico, y la instruccion inicial m
 
 --    i de fn  m      suma i^m m veces
 fm :: Int -> Int -> Int
 fm i 1 = i  --i¹
 fm i m = i^m + fm i (m-1)
+--repite i elevada a m, m veces
