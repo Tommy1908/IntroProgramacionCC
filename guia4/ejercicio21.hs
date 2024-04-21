7f3c734fcb9aecb2241d0c2b1a44c1524311ad1b
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-21 18:47:30 -0300

    Create ejercicio21.hs

---
+++ guia4/ejercicio21.hs
@@ -0,0 +1,7 @@
+--Ejercicio 21. Especificar e implementar una funci´on pitagoras :: Integer ->Integer ->Integer ->Integer
+--que dados m, n , r ∈ N≥0, cuente cuantos pares (p, q) con 0 ≤ p ≤ m y 0 ≤ q ≤ n satisfacen que p^2 + q^2 ≤ r^2
+{-
+Por ejemplo:
+pitagoras 3 4 5 ⇝ 20
+pitagoras 3 4 2 ⇝ 6
+-}
