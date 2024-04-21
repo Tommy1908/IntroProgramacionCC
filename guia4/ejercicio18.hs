e88273baf03b83d1877466091ee97ab4dcd183e2
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-21 18:08:50 -0300

    Ejercicio 18 terminado

--- guia4/ejercicio18.hs
+++ guia4/ejercicio18.hs
@@ -10,10 +10,14 @@ asegura: { resultado es el mayor de los digitos pares de n. Si n no tiene ningun
 -- 888 = 8
 -- 111 = -1
 
+mayorDigitoPar :: Int -> Int
+mayorDigitoPar x | digito == 0 = -1 --Si me devuelve 0 significa que no habia pares
+                 | otherwise = digito
+                  where digito = (mayorDigito (soloPares x 0))
 
 
 
---    x      i         con i empezando en 0
+--             x      i         con i empezando en 0 para poder asignar posiciones
 soloPares :: Int -> Int -> Int
 soloPares x i | mod ultimoDigito 2 == 0 && x < 10 = x * (10^i)
               | mod ultimoDigito 2 == 0 = ultimoDigito * (10^i) + soloPares (sacarUltimoDigito x) (i+1)
@@ -21,7 +25,17 @@ soloPares x i | mod ultimoDigito 2 == 0 && x < 10 = x * (10^i)
                   where ultimoDigito = mod x 10
 --Devuelve el numero con solo digitos pares. x * (10^i) pone el digito en posicion correcta
 
+
+
+mayorDigito :: Int -> Int
+mayorDigito x | x < 10 = x
+                | ultimoDigito <= anteUltimoDigito = mayorDigito (sacarUltimoDigito x)
+                | ultimoDigito > anteUltimoDigito = mayorDigito( (sacarUltimoDigito x) - anteUltimoDigito + ultimoDigito) --ej 1234 -> 123 - 3 + x = 12X 
+                  where ultimoDigito = mod x 10
+                        anteUltimoDigito = div (mod x 100) 10
+--Devuelve el mayor digito
+
+
+
 sacarUltimoDigito :: Int -> Int
 sacarUltimoDigito x = div x 10
-
-mayorDigito
