b2a9e7e55dd9af49cb6a6ee33e224838e934c185
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-21 18:47:14 -0300

    PREGUNTAR

--- guia4/ejercicio20.hs
+++ guia4/ejercicio20.hs
@@ -1,2 +1,7 @@
---Ejercicio 20. Especificar e implementar la funcion tomaValorMax :: Int ->Int ->Int
+--Ejercicio 20. Especificar e implementar la funcion tomaValorMax :: Int -> Int -> Int
 --que dado un numero entero n1 ≥ 1 y un n2 ≥ n1 devuelve algun m entre n1 y n2 tal que sumaDivisores(m) = max{sumaDivisores(i) | n1 ≤ i ≤ n2}
+
+esDivisible :: Int -> Int -> Bool
+esDivisible x y | x == 0 = True --Llegaste perfecto
+                | x < 0 = False --Se paso
+                | otherwise = esDivisible (x-y) y --A 'a' ir restando 'b' (dividir como en primaria)
