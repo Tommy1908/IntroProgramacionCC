bc6eab163c0ca7951ddcd2ca0d687afdeacb82cd
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-21 20:46:45 -0300

    Ejercicio 21

--- guia4/ejercicio21.hs
+++ guia4/ejercicio21.hs
@@ -1,7 +1,23 @@
---Ejercicio 21. Especificar e implementar una funci´on pitagoras :: Integer ->Integer ->Integer ->Integer
+--Ejercicio 21. Especificar e implementar una funcion pitagoras :: Int -> Int -> Int -> Int
 --que dados m, n , r ∈ N≥0, cuente cuantos pares (p, q) con 0 ≤ p ≤ m y 0 ≤ q ≤ n satisfacen que p^2 + q^2 ≤ r^2
 {-
 Por ejemplo:
 pitagoras 3 4 5 ⇝ 20
 pitagoras 3 4 2 ⇝ 6
 -}
+
+--m,n,r son limites, hay que probar las combinaciones menores
+--            m      n      r
+pitagoras :: Int -> Int -> Int -> Int
+pitagoras m n r = valoresX m n r
+
+valoresX :: Int -> Int -> Int -> Int
+valoresX x y r | x < 0 = 0
+               | otherwise = valoresY x y r + valoresX (x-1) y r
+
+valoresY :: Int -> Int -> Int -> Int
+valoresY x y r | y < 0 = 0
+               | x^2 + y^2 <= r^2 = 1 + valoresY x (y-1) r
+               | otherwise = valoresY x (y-1) r
+
+--Prueba todas las combinaciones de m y n de hasta 0, donde cumpla pitagoras, y las cuenta
