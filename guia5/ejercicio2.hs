4085fdd655d0ceb7100c11625d6c079c4eae69ed
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-22 19:47:33 -0300

    Update ejercicio2.hs

--- guia5/ejercicio2.hs
+++ guia5/ejercicio2.hs
@@ -32,24 +32,3 @@ quitar _ [] = []
 quitar n (x:xs) | n == x = xs
                 | otherwise = x:quitar n xs
 
-----------------------------------------------------
-maximo :: [Int] -> Int  -- No lista vacia
-maximo (x:xs) | xs == [] = x
-              | otherwise = esMaximo x (maximo xs)
-
-esMaximo :: Int -> Int -> Int
-esMaximo x y | x >= y = x
-             | otherwise = y
-
---maximoProfe :: Int -> Int -> Int
---maximoProfe [x] = x
---maximoProfe(x,y,xs)| x>y = maximo(x:xs)
---                   | otherwise = maximo(y:xs) 
-----------------------------------------------------
---Seria mejor usar quitar todos pero no lo tengo ahora, asi que desp lo cambiamos
---ordenar :: [Int] -> [Int] --De menor a mayor ERA! ESTA MAL
---ordenar [] = []
---ordenar (x:xs) = 
-    --ordenar (quitar maximo(x:xs) (x:xs)) ++ maximo(x:xs)
-    --maximo(x:xs) : ordenar(quitar (maximo(x:xs)) (x:xs))
- 
