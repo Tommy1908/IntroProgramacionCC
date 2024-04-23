1a27c70af88e3cf7f956a753f9eeb056df230410
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-23 15:23:30 -0300

    Update ejercicio3.hs

--- guia5/ejercicio3.hs
+++ guia5/ejercicio3.hs
@@ -1,16 +1,19 @@
-----------------------------------------------------
-maximo :: [Int] -> Int  -- No lista vacia
-maximo (x:xs) | xs == [] = x
-              | otherwise = esMaximo x (maximo xs)
+--Ejercicio 3. Definir las siguientes funciones sobre listas de enteros--
 
-esMaximo :: Int -> Int -> Int
-esMaximo x y | x >= y = x
-             | otherwise = y
+sumatoria :: [Int] -> Int --Suma cada elemento de la lista | No hace falta poner longitud, ya que cumple con lo pedido igual
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
 
---maximoProfe :: Int -> Int -> Int
---maximoProfe [x] = x
---maximoProfe(x,y,xs)| x>y = maximo(x:xs)
---                   | otherwise = maximo(y:xs) 
+productoria :: [Int] -> Int --Multiplica cada elemento de la lista
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
+
+maximo :: [Int] -> Int --No lista vacia
+maximo [x] = x
+maximo (x:xs) | x > maximo xs = x
+              | otherwise = maximo xs
+
+
 ----------------------------------------------------
 --Seria mejor usar quitar todos pero no lo tengo ahora, asi que desp lo cambiamos
 --ordenar :: [Int] -> [Int] --De menor a mayor ERA! ESTA MAL
