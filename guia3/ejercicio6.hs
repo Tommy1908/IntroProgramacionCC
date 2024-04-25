679c2555fae8ba317cb0cd7521e3251e51b28813
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-25 18:29:04 -0300

    Correccion Ejercicio6

--- guia3/ejercicio6.hs
+++ guia3/ejercicio6.hs
@@ -10,5 +10,5 @@ bisiesto 1900 ⇝ False, bisiesto 2000 ⇝ True.
 --}
 
 bisiesto :: Int -> Bool
-bisiesto x| mod x 4 == 0 || (mod x 100 == 0 && mod x 400 == 0) = True
-          | otherwise = False
+bisiesto x | (mod x 100 == 0) = (mod x 400 == 0)
+           | otherwise = (mod x 4 == 0)
