e593b03668184e304f724c391df5ba4af7b274a2
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-25 17:47:10 -0300

    Ejercicio 5 terminado

--- guia5/ejercicio5.hs
+++ guia5/ejercicio5.hs
@@ -31,3 +31,43 @@ longitud (x:xs) = 1 + longitud xs
 
 
 
+--2)
+--Elementos de s > 2
+
+descomponerEnPrimos :: [Int] -> [[Int]]
+descomponerEnPrimos [] = []
+descomponerEnPrimos (x:xs) = [(descomponerEnPrimosAux x 1)] ++ (descomponerEnPrimos xs)
+
+--                         x     k=1
+descomponerEnPrimosAux :: Int -> Int -> [Int]
+descomponerEnPrimosAux 1 _ = []
+descomponerEnPrimosAux x k | (mod x (nEsimoPrimo k))== 0 = [nEsimoPrimo k] ++ (descomponerEnPrimosAux (div x (nEsimoPrimo k)) k)
+                           | otherwise = descomponerEnPrimosAux x (k+1)
+
+--Si el numero es divisible por el primo k, entonces ponemos a k en la lista, y volvemos a correr el programa con el numero divido k, y k devuelta por si pudera volver a dividirlo.
+--En otro caso, si no puede dividirlo, probamos con el siguiente primo. Cuando eventualmente quede 1 en la division, terminamos.
+
+---Funciones de primos del ej 16-guia 4---
+
+--               >1
+menorDivisor :: Int -> Int
+menorDivisor x| mod x 2 == 0 = 2  --Todo numero par su menor divisor es 2
+              | otherwise = encontrarDivisorDesde 2 x
+
+--Devuelve i, cuando sea el menor divisor
+encontrarDivisorDesde :: Int -> Int -> Int
+encontrarDivisorDesde i x | mod x i == 0 = i
+                          | otherwise = encontrarDivisorDesde (i+1) x
+
+esPrimo :: Int -> Bool
+esPrimo x = x == menorDivisor x
+
+nEsimoPrimo :: Int -> Int
+nEsimoPrimo n = encontrarPrimo 2 1 n
+
+--                p      i      n
+encontrarPrimo:: Int -> Int -> Int -> Int
+encontrarPrimo p i n | esPrimo p && i == n = p
+                     | esPrimo p = encontrarPrimo (p+1) (i+1) n
+                     | otherwise = encontrarPrimo (p+1) i n
+-- p es el primo de pruebas, i un contador, y n el contador objetivo
