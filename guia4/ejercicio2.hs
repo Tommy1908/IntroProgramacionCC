a8428487543775cadc398eae523372966c6f17aa
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-04-28 16:17:40 -0300

    Correcciones
    El ej2 tenia un error para el caso (-0.5)

--- guia4/ejercicio2.hs
+++ guia4/ejercicio2.hs
@@ -8,11 +8,11 @@
 --Solo para Positivos--
 parteEntera :: Float -> Int
 parteEntera x | 0 < x && x < 1 = 0
-              | otherwise = 1 + parteEntera1 (x-1)
+              | otherwise = 1 + parteEntera (x-1)
 
 --Para Positivos y Negativos--
 parteEnteraFull :: Float -> Int
-parteEnteraFull x | (-1) < x && x < 1 = 0
---parteEnteraFull x | (abs x) < 1 = 0
+parteEnteraFull x | 0 < x && x < 1 = 0
+                  | 0 > x && x > (-1) = -1     
                   | x > 0 = 1 + parteEnteraFull (x-1)
                   | x < 0 = (-1) + parteEnteraFull (x+1)
