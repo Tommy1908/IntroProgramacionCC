1e17d11e62107f7adff5570702e584062347a77e
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-05-02 16:41:36 -0300

    2doCuatri2023_1 Parcial

--- practicaParciales/2doCuatri2023_1.hs
+++ practicaParciales/2doCuatri2023_1.hs
@@ -22,7 +22,7 @@ problema porcentajeDeVotosAfirmativos (formulas: seq⟨String x String⟩,votos:
  requiere: {La suma de todos los elementos de votos es menor o igual a cantTotalVotos}
  asegura: {res es el porcentaje de votos no blancos (es decir, asociados a alguna de las fórmulas) sobre el total de votos emitidos}
 }
-Para resolver este ejercicio pueden utilizar la siguiente función que devuelve como Float la división entre dos números de tipo Int:
+Para resolver este ejercicio pueden utilizar la siguiente función que devuelve como Float la división entre dos números de tipo Int
 
 ------------------------------------------
 
@@ -30,7 +30,8 @@ Para resolver este ejercicio pueden utilizar la siguiente función que devuelve 
 2) Formulas Inválidas [3 puntos]
 problema formulasInvalidas (formulas: seq⟨String x String⟩) : Bool {
  requiere: {True}
- asegura: {(res = true) <=> formulas no contiene nombres repetidos, es decir que cada candiadato esta en mas de una formula (no puede ser candidato a presidente y a vicepresidente ni en la misma formula ni en formulas distintas)  }
+ asegura: {(res = true) <=> formulas no contiene nombres repetidos, es decir que cada candiadato esta en mas de una formula 
+           (no puede ser candidato a presidente y a vicepresidente ni en la misma formula ni en formulas distintas)  }
 ------------------------------------------
 
 
@@ -56,9 +57,10 @@ problema menosVotado (formulas: seq⟨String x String⟩, votos:seq< Z >) : Stri
  requiere: {|formulas| > 0}
  asegura: {res es el candidato a presidente de formulas menos votado de acuerdo a los votos contabilizados en votos}
 }-}
+
+
 {-A continuación te dejamos una estructura básica para resolver los ejercicios.
 Este código no pretende resolver ningun caso de los ejercicios planteados, es sólo una plantilla.-}
-
 
 {-
 -- Ejercicio 1
@@ -85,6 +87,7 @@ menosVotado _ _ = ""
 
 
 -----------------------------------------------------------------------------------------------------------
+
 --Empiezo por el ejercicio 2 para
 
 l1 = [("Miguel","Marcos"),("Miriam","Julian"),("Sam","Mateo"),("Lucas","Pablo")] --Valida
@@ -108,7 +111,67 @@ elementosDuplicados (a,b) | a==b = True
 
 --Ej 1
 
+--req: {¬formulasInvalidas(formulas)}
+--req: {|formulas| = |votos|}
+--req: {Todos los elementos de votos son mayores o iguales a 0}
+--req: {La suma de todos los elementos de votos es menor o igual a cantTotalVotos} (el ulitmo parametro, entiendo que hay q sumarlos a mano porque sino seria otra funcion)
+--as: {res es el porcentaje de votos no blancos (es decir, asociados a alguna de las fórmulas) sobre el total de votos emitidos} totalVotos/votosACandidatos
+
 division :: Int -> Int -> Float
 division a b = (fromIntegral a) / (fromIntegral b)
 
+
+f1 = [("Miguel","Marcos"),("Miriam","Julian"),("Sam","Mateo"),("Lucas","Pablo")]
+v1 = [98,34,14,64] :: [Int]
+--210 = 100% 
+v2 = [0,304,0,74] :: [Int]
+--378 = 100%
+
 porcentajeDeVotosAfirmativos :: [(String, String)] -> [Int] -> Int  -> Float
+porcentajeDeVotosAfirmativos [] [] 0 = 100 --Yo entiendo que si no hubo votos en blanco, y tampoco hubo votados, el 100% de los votos (0) fueron efectivos, no blancos
+porcentajeDeVotosAfirmativos [] [] _ = 0   --Yo entiendo que si no hubo votados, pero hubo votos, el 0% fueron efectivos porque no fueron a ningun candidato 
+porcentajeDeVotosAfirmativos formulas votos votosTotales = (division (sumaVotosAfirmativos formulas votos) votosTotales)*100
+
+sumaVotosAfirmativos :: [(String, String)] -> [Int] -> Int 
+sumaVotosAfirmativos [] [] = 0 --Se supone que igualmente no llegan casos []
+sumaVotosAfirmativos (x:xs) (y:ys) | xs /= [] = y + sumaVotosAfirmativos xs ys
+                                   | otherwise = y
+
+-----------------------------------------------
+
+--Ej 3
+
+--req: {La segunda componente de algún elemento de formulas es vice}
+--req: {¬formulasInvalidas(formulas)}
+--req: {|formulas| = |votos|}|
+--req: {Todos los elementos de votos son mayores o iguales a 0}
+--req: {Hay al menos un elemento de votos mayores estricto a 0}
+--as: {res es el porcentaje de votos que obtuvo vice sobre el total de votos afirmativos}
+ 
+--                    vice         formulas          votos
+porcentajeDeVotos :: String -> [(String, String)] -> [Int] -> Float
+porcentajeDeVotos vice formulas votos = division (votosVice vice formulas votos) (sumaVotosAfirmativos formulas votos) 
+
+votosVice :: String -> [(String, String)] -> [Int] -> Int
+votosVice vice [] [] = 0
+votosVice vice ((x,y):xs) (v:vs) | vice == y = v 
+                                 | otherwise = votosVice vice xs vs
+
+
+-----------------------------------------------
+
+--Ej 4
+
+--req: {¬formulasInvalidas(formulas)}
+--req: {|formulas| = |votos|}
+--req: {Todos los elementos de votos son mayores o iguales a 0}
+--req: {Hay al menos un elemento de votos mayores estricto a 0}
+--req: {|formulas| > 0}
+--as: {res es el candidato a presidente de formulas menos votado de acuerdo a los votos contabilizados en votos}
+--Asumo que si hay dos con la menor cantidad devuelvo cualquiera
+
+--                  formulas         votos
+menosVotado :: [(String, String)] -> [Int] -> String
+menosVotado [(x,y)] [v] = x
+menosVotado ((x,y):(a,b):xs) (v:v2:vs) | v <= v2 = menosVotado ((x,y):xs) (v:vs)
+                                       | otherwise = menosVotado ((a,b):xs) (v2:vs)
