20ae7dcd245ae76de2a58149c19f2262fa1ab9ac
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-05-22 11:23:59 -0300

    avance

--- guia7/ejercicio1.py
+++ guia7/ejercicio1.py
@@ -58,10 +58,29 @@ def ordenados(s:list[int]) -> bool:
                 res = False
     return res
 
+#5) Dada una lista de palabras, devolver verdadero si alguna palabra tiene longitud mayor a 7
 
-print(ordenados([1,2,3,4,5]))
-print(ordenados([1,2,6,9]))
-print(ordenados([1,2,3,3,5]))
-print(ordenados([6,7,1,0]))
-print(ordenados([1,2,3,23]))
+def palabrasLargas(l:list[str]) -> bool:
+    res:bool = False
+    for i in range(len(l)):
+        if len(l[i]) > 7:
+            res = True
+    return res
+
+#6) Dado un texto en formato string, devolver verdadero si es palindromo (se lee igual en ambos sentidos), falso en caso contrario
+#Seria como ver si es capicua. No tengo en cuenta las mayusculas o minusculas, no se si es necesario
+def esPalindromo(palabra:str) -> bool:
+    res:bool = False
+    for i in range(len(palabra)):
+        if palabra == reverso(palabra):
+            res = True
+    return res
+
+def reverso(palabra:str) -> str:
+    res:str = ""
+
+    for i in range(len(palabra)):
+        res = palabra[i] + res
+
+    return res
 
