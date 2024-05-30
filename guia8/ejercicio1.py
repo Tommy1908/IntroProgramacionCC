0d8d1b4cec2d4772a41efb7538ae24562ca437f1
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-05-29 22:25:54 -0300

    reordenamiento

--- guia8/ejercicio1.py
+++ guia8/ejercicio1.py
@@ -1,46 +1,47 @@
-#Ejercicio 1
+########################### Archivos ###########################
+import typing
 
-def leer_lineas(archivo)-> list[str]:
-    archivo = open("ejercicio1.txt")
-    lineas = archivo.readlines()
+def rel(): exec(f"import {__name__};import importlib;importlib.reload({__name__})")
+r = f"rel(); from {__name__} import *"
+print("OK. Reload with exec(r)")
+
+##################### Funciones Utiles ######################### 
+
+def leer_lineas(ruta)-> list[str]: #Devuelve una lista con cada linea por elemento
+    archivo: typing.IO = open(ruta)
+    lineas: list[str] = archivo.readlines()
     archivo.close()
     return lineas
 
-def leer(archivo) -> str:
-    archivo = open("ejercicio1.txt")
-    contenido = archivo.read()
+def leer(ruta) -> str: #Devuelve todo el contenido del archivo en string
+    archivo: typing.IO = open(ruta)
+    contenido: str = archivo.read()
     archivo.close()
     return contenido
 
-#1) Una funcion contar lineas(in nombre archivo : str) → int que cuenta y devuelva la cantidad de lineas de texto del archivo dado.
+################################################################
 
-def contar_lineas(archivo:str) -> int:
-    lineas:list[str] = leer_lineas(archivo)
+######################### Ejercicio 1 ##########################
+
+
+#1)
+def contar_lineas(ruta:str) -> int:
+    lineas:list[str] = leer_lineas(ruta)
     return len(lineas)
 
-#print(contar_lineas("ejercicio1.txt"))
-
-#2) Una funcion existe palabra(in palabra : str, in nombre archivo : str) → bool que dice si una palabra existe en un archivo de texto o no
-
-def existe_palabra(palabra:str,archivo:str) -> bool:
-    contenido:str = leer(archivo)
-    res:bool = False
-
-    if contenido.count(palabra) > 0:
-        res = True
-
+def contar_lineas_alternativo(ruta:str) -> int:
+    contenido = leer(ruta)
+    res:int = 1 #Empieza en 1 porque siempre hay por lo menos 1 linea
+    for i in range(len(contenido)):
+        if contenido[i] == '\n':
+            res += 1
     return res
 
-print(existe_palabra("es","ejercicio1.txt"))
-#Cualquier aparicion de la palabra, incluso dentro de otra es contada.
+#2) #Voy a considerar palabra todo aquello que este separado de otro caracter no vacio o un salto de linea
+def existe_palabra(palabra:str,ruta:str) -> bool:
+    contenido = leer(ruta)
+    enPalabra:bool = False
+    palabras:list[str] = []
 
-#3)Una funcion cantidad apariciones(in nombre archivo : str, in palabra : str) → int 
-#que devuelve la cantidad de apariciones de una palabra en un archivo de texto
-
-def cantidad_de_apariciones(palabra:str,archivo:str) -> int:
-    contenido:str = leer(archivo)
-
-    return contenido.count(palabra)
-
-print(cantidad_de_apariciones("es","ejercicio1.txt"))
-
+    #for i in range(1,len(contenido)-1):
+    #    if contenido[i] != ' ':
