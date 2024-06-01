ae8b32de84e132124df8a73c9821e85bf6c2fe50
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-06-01 17:10:29 -0300

    Ej 17, 18

--- guia8/colas.py
+++ guia8/colas.py
@@ -82,5 +82,83 @@ def armar_secuencia_de_bingo() -> Cola[int]:
     print(numeros_para_bingo)
     return c
 
-c = armar_secuencia_de_bingo()
-descolar(c)
+#def jugar_carton_de_bingo(carton: list[int], bolillero: Cola[int]) -> int:
+
+################################################################
+
+######################## Ejercicio 17 ##########################
+#Voy a hacer esto para poner colas para probar el codigo
+def lista_pacientes_a_cola(l:list[(int,str,str)]) -> Cola[(int,str,str)]: 
+    c = Cola()
+    for i in range(len(l)):
+        c.put(l[i])
+    return c
+
+def n_pacientes_urgentes(c: Cola[(int,str,str)]) -> int:  #Hay q devolver pacientes que ntengan el int con rango [1,3]
+    #Desarmo la cola y leo los datos
+    pacientes_datos: list[(int,str,str)] = []
+    pacientes_urgentes: int = 0
+
+    while not(Cola.empty(c)):
+        pacientes_datos.append(c.get())
+    print(pacientes_datos)
+    #Armo devuelta la cola y aprovecho el mismo for para ver cuantos pacientes estan entre 1 y 3
+    for i in range(len(pacientes_datos)):
+        #Armar cola
+        c.put(pacientes_datos[i])
+        #Ver pacientes
+        if pacientes_datos[i][0] <= 3: #aca asumo que los valores siempre son entre el 1 y el 10
+            pacientes_urgentes += 1
+    
+    return pacientes_urgentes
+
+################################################################
+
+######################## Ejercicio 18 ##########################
+
+#requiere = true
+#asegura = Si no hay personas devuelve la cola vacia
+#asegura = La cola devuelta tiene a todos las personas en la cola dada segun el orden de entrada a la cola (primero los primeros) en 3 grupos,
+#prioritarios, preferencial y el resto, cada subgrupo con su orden de entrada a la cola
+
+#Primero voy a hacer lo mismo que antes y que dada una lista armar una cola para probar el codigo luego
+def lista_clientes_a_cola(l:list[(str,int,bool,bool)]) -> Cola[(str,int,bool,bool)]: 
+    c = Cola()
+    for i in range(len(l)):
+        c.put(l[i])
+    return c
+#Otra opcion podria ser armar listas diferentes y asignarles un valor general de llegada
+def atencion_a_clientes(c: Cola[(str,int,bool,bool)]) -> Cola[(str,int,bool,bool)]:
+    #Desarmemos la cola en una lista
+    cola_datos: list[(str,int,bool,bool)] = []
+    while not(Cola.empty(c)):
+        cola_datos.append(c.get()) #El orden de llegada es correspondiente a su index
+    print(cola_datos)
+
+    #Armamos la cola devuelta
+    for i in range(len(cola_datos)):
+        c.put(cola_datos[i])
+
+    #Ahora creamos una nueva cola para devolver(la inicial es de tipo in) y vemos el orden. Metemos primero en la lista para ver que no haya repetidos
+    res = Cola()
+    res_lista: list[(str,int,bool,bool)] = []
+
+    for i in range(len(cola_datos)):
+        if cola_datos[i][3] == True:
+            res_lista.append(cola_datos[i])
+
+    for i in range(len(cola_datos)):
+        if (cola_datos[i][2] == True ) and (cola_datos[i] not in res_lista): #Usar un pertenece si no se puede usar in
+            res_lista.append(cola_datos[i])
+            
+    for i in range(len(cola_datos)):
+        if cola_datos[i] not in res_lista:
+            res_lista.append(cola_datos[i])
+
+    #Por ultimo metemos estos datos finales a la cola
+    for i in range(len(res_lista)):
+        res.put(res_lista[i])
+            
+    return res
+
+################################################################
