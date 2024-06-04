1cff33e6b5e5cd357ede51322a333eea3a424036
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-06-04 16:55:53 -0300

    ej 20

--- guia8/diccionarios.py
+++ guia8/diccionarios.py
@@ -1,6 +1,7 @@
 ######################## Diccionarios ##########################
 from queue import LifoQueue as Pila
 import random
+import typing
 ##################### Funciones Utiles ######################### 
 
 ################################################################
@@ -36,6 +37,51 @@ def agrupar_por_longitud(ruta:str) -> dict:
 
     print(d.items())
 
+################################################################
 
-agrupar_por_longitud("ejercicio19.txt")
+######################## Ejercicio 20 ##########################
+#Voy a copiar la logica para separar los datos
 
+def calcular_promedio_por_estudiante(ruta:str) -> dict[str,float]:
+    archivo: typing.IO = open(ruta)
+    contenido: list[str] = archivo.readlines()
+    #Saco todos los valores que no sean de ese lu de contenido
+    contenido_en_lista: list[list[str]] = []
+    for i in range(len(contenido)):
+        temp:str = ""
+        dato: list[str] = []
+        for k in range(len(contenido[i])):
+            if contenido[i][k] != ',' and k != len(contenido[i])-1:
+                temp += contenido[i][k]
+            else:
+                dato += [temp]
+                temp = ""
+        contenido_en_lista += [dato]
+
+    #Busco todos los lues
+    lues:list[str] = []
+    for i in range(len(contenido_en_lista)):
+        if contenido_en_lista[i][0] not in lues:
+            lues += [contenido_en_lista[i][0]]
+    print(lues)
+
+    #Calculo promedio para cada lu, correspondiendo su indice
+    promedios:list[float] = []
+    for i in range(len(lues)):
+        promedio:float = 0
+        cantidad:int = 0
+        for k in range(len(contenido_en_lista)):
+            if contenido_en_lista[k][0] == lues[i]:
+                promedio += int(contenido_en_lista[k][3] )
+                cantidad += 1
+        promedio /= cantidad
+
+        promedios.append(promedio)
+    print(promedios)
+    #Por ultimo los meto a un diccionario
+    d: dict[str,float] = {}
+    for i in range(len(lues)):
+        d[lues[i]] = promedios[i]
+    return d
+
+print(calcular_promedio_por_estudiante("ejercicio20.csv"))
