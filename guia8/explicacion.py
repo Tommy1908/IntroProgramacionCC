067c88212461aa44fcdc6503972809a26ed56422
Tomas Villafañe <77515268+Tommy1908@users.noreply.github.com>
2024-06-08 13:22:09 -0300

    Update explicacion.py

--- guia8/explicacion.py
+++ guia8/explicacion.py
@@ -6,7 +6,39 @@
 # '\n' es un solo caracter
 
 
-archivo = open("explicacion.txt")
-contenido = archivo.read()
-print(contenido)
-archivo.close
+#archivo = open("explicacion.txt")
+#contenido = archivo.read()
+#print(contenido)
+#archivo.close
+
+
+def separar_palabras(contenido:str,caracteresDistintos:list[str]) -> list[list[str]]:
+    resultado: list[list[str]] = []
+    expresion :list[str] = []
+    palabra = "" 
+    for i in range(len(contenido)):
+        if len(expresion) < 4:
+            caracterActual = contenido[i]
+            if caracterActual in caracteresDistintos:
+                expresion.append(palabra)
+                palabra = ""
+            else:
+                palabra += caracterActual
+        else:
+            caracterActual = contenido[i]
+            resultado.append(expresion)
+            expresion = []
+            caracterActual = contenido[i]
+            if caracterActual in caracteresDistintos:
+                expresion.append(palabra)
+                palabra = ""
+            else:
+                palabra += caracterActual
+                
+    if palabra != "":
+        expresion.append(palabra)
+    if expresion != []:
+        resultado.append(expresion)
+    return resultado
+
+print(separar_palabras("aa,bb,cc,dd,TO,ff,gg,hh",[","]))
